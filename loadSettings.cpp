662d9e201706482cbb8628a4f144a0797dbd5c09
Samuel Rey <samuelrp12@educastur.es>
2020-12-26 20:01:18 +0100

    fixed array size for loadedTypes

--- loadSettings.cpp
+++ loadSettings.cpp
@@ -14,7 +14,7 @@ namespace plugInSettings {
 		int xlocation[] = { 293,293,291,309,415,191,191,1 ,1  ,0,0,583,471 };
 		int ylocation[] = { 68 ,45 ,1  ,114,418,62 ,92 ,1 ,121,0,0 ,157,95 };
 		int fieldHeight[] = { 44 ,22 ,22 ,22 ,22 ,22 ,22 ,22,22 ,0,0 ,22,22 };
-		for (int i = 0; i < 12; i++) {
+		for (int i = 0; i < 13; i++) {
 			loadedTypes[0].fields.push_back(stripField());
 			loadedTypes[0].fields[i].fieldXLocation = xlocation[i];
 			loadedTypes[0].fields[i].fieldYLocation = ylocation[i];
