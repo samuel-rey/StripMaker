ee229f0f4a38bd5bf8ffb91ca9a971bfd288c145
Samuel Rey <samuelrparaja@gmail.com>
2020-12-26 03:05:13 +0100

    added constants file

---
+++ constant.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "pch.h"
+// ES tag and functions codes
+const int TAG_ITEM_PRINT_STATUS = 100;
+const int TAG_FUNC_PRINT_STRIP = 101;
+
+// field types codes
+const int FIELD_CALLSIGN = 0;
+const int FIELD_PHONETIC_CALLSIGN = 1;
+const int FIELD_AIRCRAFT_TYPE = 2;
+const int FIELD_SQWAWK = 3;
+const int FIELD_TAS = 4;
+const int FIELD_RFL = 5;
+const int FIELD_RULES = 6;
+const int FIELD_SLOT = 7;
+const int FIELD_EDT = 8;
+const int FIELD_ADEP = 9;
+const int FIELD_ROUTE = 10;
+const int FIELD_ADES = 11;
+const int FIELD_ADEPROUTE = 12;
