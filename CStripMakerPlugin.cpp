cbd882bb51bba39edda0d7c1b8b89efd71bd57c6
Samuel Rey <samuelrparaja@gmail.com>
2020-12-26 02:50:59 +0100

    set log flushing policy

--- CStripMakerPlugin.cpp
+++ CStripMakerPlugin.cpp
@@ -9,6 +9,8 @@ std::shared_ptr<spdlog::logger> logger;
 CStripMakerPlugIn::CStripMakerPlugIn(void) :CPlugIn(EuroScopePlugIn::COMPATIBILITY_CODE, MY_PLUGIN_NAME, MY_PLUGIN_VERSION, MY_PLUGIN_DEVELOPER, MY_PLUGIN_COPYRIGHT)
 {
 	auto logger = spdlog::basic_logger_mt("StripMaker", "StripMaker/logs/log.txt");
+	spdlog::flush_every(std::chrono::seconds(5));
+	spdlog::flush_on(spdlog::level::err);
 	logger->info("StripMaker plugin starting up...");
 	plugInSettings::loadSettings();
 	logger->info("Succesfully loaded {} strip types", plugInSettings::getTypes().size());
